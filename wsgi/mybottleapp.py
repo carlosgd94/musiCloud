20ffb44acbb32e62da7feb6ea5a1795cf57e06ac
Carlos Gómez <carlos.gomez.diaz@outlook.com>
2016-06-17 12:39:09 +0200

    cambio mybotteapp

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -27,13 +27,15 @@ def search():
 @route('/letra', method="post")
 def letra():
     artist2 = request.forms.get("artista2")
-    cancion = request.forms.get("cancion")
-    dicc_peticion = {'artist':artist2,'song':cancion,'fmt':'xml'}
+    cancionk = request.forms.get("cancion")
+    dicc_peticion = {'artist':artist2,'song':cancionk,'fmt':'xml'}
     peticion = requests.get('http://lyrics.wikia.com/api.php?', params=dicc_peticion)
+    print peticion.url
     datos1 = etree.fromstring(peticion.text.encode("utf-8"))
     url = datos1.find("url")
     lyrics = datos1.find("lyrics")
     song = datos1.find("song")	
+    
     return template('letra', url=url.text,lyrics=lyrics.text,song=song.text)
 
 
